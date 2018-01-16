ebc52e60bb9e32c5567c113cf82ce3fee05e7001
LuisAMatamoros <33907915+LuisAMatamoros@users.noreply.github.com>
2018-01-15 17:53:01 -0800

    Added Prototype
    Not working yet. Will look into sling shot later

---
+++ Phoenix Unity Project/Assets/Scripts/Swipe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeControls : MonoBehaviour
+{
+    private bool Tap, SwipeLeft, SwipeRight, SwipeUp, SwipeDown;
+    private Vector2 StartTouch, SwipeDelta;
+
+    private void Update()
+    {
+        Tap = SwipeLeft= SwipeUp= SwipeDown= SwipeRight = false;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Tap = true;
+            StartTouch = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0)) //OR DOWN
+        {
+            Reset();
+        }
+    }
+
+    private void Reset()
+    {
+        StartTouch = SwipeDelta =Vector2.zero;
+    }
+
+    public Vector2 swipeDelta { get { return SwipeDelta; } }
+    public bool swipeLeft { get{ return SwipeLeft; } }
+    public bool swipeRight { get { return SwipeRight; } }
+    public bool swipeUp { get { return SwipeUp; } }
+    public bool swipeDown { get { return SwipeDown; } }
+
+
+
+
+
+}
